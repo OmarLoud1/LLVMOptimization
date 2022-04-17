d8ddbbb2d7045920ac177ce6c59a248cdc54abc8
OmarLoud1 <62630012+OmarLoud1@users.noreply.github.com>
2022-04-16 23:04:54 -0400

    got deeper into the AST and for loop expression

--- example_pass.cpp
+++ example_pass.cpp
@@ -7,12 +7,24 @@ void ExamplePass(ModuleAST* TheModule) {
 
   //fprintf(stderr, "This Module has %lu externs and %lu functions!\n\n", TheModule->Externs.size(), TheModule->Functions.size());
 
+  
   for (auto const& x : TheModule->Functions){
   
-      fprintf (stderr, "%s  \n", x.first.c_str());  
-      std::pair<std::unique_ptr<FunctionAST>, bool> pair1;
-      pair1 = TheModule->Functions.find(x.first);
-      //  
+      fprintf (stderr, "%s %p  \n", x.first.c_str(), x.second.first.get());
+      FunctionAST* func = x.second.first.get();
+      ForExprAST* forloop;
+      ExprAST* exp = func->Body.get();
+
+      if(typeid(exp)== typeid(forloop)){
+        exp = (ForExprAST*) exp;
+        forloop = (ForExprAST*) exp;
+        ExprAST* body1 = forloop->Body.get();
+      }
+      fprintf (stderr, "%s %s  \n", func, exp);
+      fprintf (stderr, "%d  \n", TheModule->getFunctionProto(x.first.c_str())->isOperator()); 
+
+      //std::pair<std::unique_ptr<FunctionAST>, bool> pair1;
+      //pair1 = TheModule->Functions.find(x.first);
       //fprintf (stderr, "%s  \n", pair1.first);
       //fprintf (stderr, "%s  \n", pair1.second.c_str());    
     }
